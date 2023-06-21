7e23b73c4f9ec3b8d2554e140065a7af10495112
Mithilesh Tiwari <Mithilesh@crowdbotics.com>
2023-06-21 10:02:19 +0530

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,5 +1,3 @@
-import { StyleSheet } from "react-native"
-
 export const globalOptions = {
   name: "morning-tree-dev-93465",
   url: "https://morning-tree-dev-93465.botics.co",
@@ -7,9 +5,9 @@ export const globalOptions = {
   screenOptions: {
     headerShown: false
   }
-}
+};
 export const modulesOptions = {
   "@modules/app-menu": {
     copy: "Routes available!"
   }
-}
+};
